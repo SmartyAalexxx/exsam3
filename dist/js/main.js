227e3cc1e0c375f4457699947f342a9fd91a136f
SmartyAalexxx <smarty.mazny@gmail.com>
2020-02-14 20:21:21 +0200

    changed other slider for section *team*

--- dist/js/main.js
+++ dist/js/main.js
@@ -7548,11 +7548,14 @@ return Popper;
     })
 
     $('.slider').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-      });
+      arrows: false,
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+
+    });
 
 
     
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -6,11 +6,14 @@
     })
 
     $('.slider').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-      });
+      arrows: false,
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+
+    });
 
 
     
